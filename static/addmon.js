6e7489f8282eea106ba22bef121fc1bbc10e0579
jackoff96 <amankachhap673@gmail.com>
2024-07-09 04:57:02 +0530

    js file modified

--- static/addmon.js
+++ static/addmon.js
@@ -1,2 +1,42 @@
 window.alert("hello jack iam playing freefire");
+window.document.getElementById()
+window.innerWidth()  //inner width of browser
+window.innerHeight()   // inner height of browser
+window.open() // open new window
+window.close() //close current window
+window.moveTo() // move the current window
+window.resizeTo() // resize the current window
+window.screen.width() //return width of screen
+window.screen.height() // return height of screen
+window.screen.availWidth()//return margin screen width
+window.screen.avaikHeight()// return margin screen height
+window.screen.colorDepth()//0return no of bits used to display one color
+window.screen.pixelDepth()// return pixel depth of screen
+window.location.href // returns the href (URL) of the current page
+window.location.hostname // returns the domain name of the web host
+window.location.pathname // returns the path and filename of the current page
+window.location.protocol // returns the web protocol used (http: or https:)
+window.location.assign() // loads a new document
+window.location.port // property returns the number of the internet host port (of the current page).
+window.history.back() // same as clicking back in the browser
+window.history.forward() // same as clicking forward in the browser
+window.navigator.cookieEnabled //property returns true if cookies are enabled, otherwise false
+window.navigator.appCodeName // property returns the application name of the browser:
+window.navigator..platform // property returns the platform name of the browser
+window.navigator.product //property returns the product name of the browser engine
+window.navigator.appVersion // property returns version information about the browser
+window.navigator.userAgent // property returns the user-agent header sent by the browser to the server
+window.navigator.language // property returns the browser's language:
+window.navigator.onLine // property returns true if the browser is online:
+window.navigator.javaEnabled() // method returns true if Java is enabled:
+window.confirm() // method can be written without the window prefix.
+window.prompt() // method can be written without the window prefix.
+window.setTimeout(function, milliseconds); // Executes a function, after waiting a specified number of milliseconds
+window.clearTimeout(timeoutVariable) //method stops the execution of the function specified in setTimeout().
+window.setTimeout(function, milliseconds); //method can be written without the window prefix.
+window.setInterval(function, milliseconds); //method repeats a given function at every given time-interval
+window.clearInterval(timerVariable) //method can be written without the window prefix.
+document.cookie // to set cookie
+
+
 
